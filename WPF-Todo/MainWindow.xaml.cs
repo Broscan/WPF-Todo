454987b791c8d6c98fe925328cc5370e6801bf71
Broscan <Calle992015@gmail.com>
2023-10-03 18:08:23 +0200

    Initial commit

---
+++ WPF-Todo/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+
+namespace WPF_Todo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (Priority _priority in Enum.GetValues(typeof(Priority)))
+            {
+                cbPriority.Items.Add(_priority);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WPF-Todo/Todo.cs
@@ -0,0 +1,22 @@
+﻿namespace WPF_Todo
+{
+    public enum Priority
+    {
+        Critical,
+        High,
+        Medium,
+        Low
+    }
+    internal class Todo
+    {
+
+        string Name { get; set; }
+
+        Priority _priority { get; set; }
+
+        private readonly string thingsTodo;
+
+
+
+    }
+}
